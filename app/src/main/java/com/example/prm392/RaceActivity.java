02cc5163f494e9f9e288641c10e31b9f2ffe8ec9
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-01-27 15:17:46 +0700

    fix(race): change ContentView to file activity_race

--- app/src/main/java/com/example/prm392/RaceActivity.java
+++ app/src/main/java/com/example/prm392/RaceActivity.java
@@ -34,7 +34,7 @@ public class RaceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_race);
 
         initViews();
 
